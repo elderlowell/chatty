1baa4436bd8b0443a4ce4e404e01a04bd4f2612d
Jonathan Lowell <elderlowell@gmail.com>
2017-06-27 19:20:42 -0600

    Completed Step 3.

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ var messages = ["Hello."];
 app.get('/messages', function(req, res, next) {
   res.status(200).json({ messages: messages });
 });
+
+app.post('/messages', function(req, res, next) {
+  messages.push(req.body.message);
+  res.status(200).json({ messages: messages });
+});
